599c33b2c2566fc7924a6cab25ffde015409ed5b
MikhailKrylov <89261789846@mail.ru>
2022-10-19 21:38:18 +0300

    trying to deploy...

--- src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -1,13 +1,12 @@
 package hexlet.code.app.controller;
 
-
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    @GetMapping("")
+    @RequestMapping(path = "")
     public String welcome() {
         return "view/welcome.html";
     }
