8a4315a1ee70a9144c672fc940e6d43ae073e4d6
MikhailKrylov <89261789846@mail.ru>
2022-10-19 21:42:59 +0300

    trying to deploy...

--- src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -1,6 +1,6 @@
 package hexlet.code.app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
