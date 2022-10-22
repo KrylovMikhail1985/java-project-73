c91405e07b6068aa2621843459adde75964aacbb
MikhailKrylov <89261789846@mail.ru>
2022-10-22 23:18:13 +0300

    trying to coonect to DB on railway

--- src/main/java/hexlet/code/app/controller/TestController.java
+++ src/main/java/hexlet/code/app/controller/TestController.java
@@ -3,6 +3,7 @@ package hexlet.code.app.controller;
 import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("myUrl")
+    String str;
     @Autowired
     private UserRepository userRepository;
     @PostMapping("/users")
@@ -24,14 +27,12 @@ public class TestController {
     @GetMapping("/")
     public String showMyENV() {
         String result = "";
-        result = result + "DEV = " + System.getenv("DEV") + "\n";
-        result = result + "PGHOST = " + System.getenv("PGHOST") + "\n";
-        result = result + "PGPORT = " + System.getenv("PGPORT") + "\n";
-        result = result + "PGUSER = " + System.getenv("PGUSER") + "\n";
-        result = result + "PGPASSWORD = " + System.getenv("PGPASSWORD") + "\n";
-        result = result + "PGDATABASE = " + System.getenv("PGDATABASE") + "\n";
-        result = result + "DATABASE_URL = " + System.getenv("DATABASE_URL") + "\n";
-        System.out.println(result);
-        return result;
+        String HOST = System.getenv("PGHOST");
+        String PORT = System.getenv("PGPORT");
+        String DATABASE = System.getenv("PGDATABASE");
+        System.out.println(str);
+        System.out.println("jdbc:postgresql://" + HOST + ":"  + PORT + "/" + DATABASE);
+        System.out.println("jdbc:postgresql://containers-us-west-29.railway.app:7337/railway");
+        return str;
     }
 }
