3c33b582ad9a5a4d02d4edb1f7d191353d3f1c7f
MikhailKrylov <89261789846@mail.ru>
2022-11-20 13:47:22 +0300

    corrected welcome cotroller

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,7 +1,5 @@
 package hexlet.code.controller;
 
-import com.rollbar.notifier.Rollbar;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
