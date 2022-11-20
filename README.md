d630897e2f33035241bbc37a890c06dc443eeff8
MikhailKrylov <89261789846@mail.ru>
2022-11-20 11:18:13 +0300

    added Rollbar for exception analyze

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-# In development...
+# Last Hexlet's project
+### In this project used:
+1. ORM Entity with different relations o2o, o2m, m2m
+2. Liquibase
+2. Authentication with Spring Security + JWT
+3. Filtration with Spring + querydsl
+4. Open API documentation
+5. Testing with mock + filling db with DBRider
 
-## java-project-73-production.up.railway.app
+## Demo: https://java-project-73-production.up.railway.app
 
+### Open Api documentation by: 
+https://java-project-73-production.up.railway.app/swagger-ui.html
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/KrylovMikhail1985/java-project-73/workflows/hexlet-check/badge.svg)](https://github.com/KrylovMikhail1985/java-project-73/actions)
 [![Java CI](https://github.com/KrylovMikhail1985/java-project-73/actions/workflows/github-actions.yml/badge.svg)](https://github.com/KrylovMikhail1985/java-project-73/actions/workflows/github-actions.yml)
---
+++ src/main/java/hexlet/code/app/config/RollbarConfig.java
@@ -0,0 +1,43 @@
+package hexlet.code.app.config;
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@Configuration()
+@EnableWebMvc
+@ComponentScan({
+
+// UPDATE TO YOUR PROJECT PACKAGE
+        "hexlet.code.app",
+        "com.rollbar.spring"
+
+})
+public class RollbarConfig {
+    @Value(value = "${ACCESS_TOKEN}")
+    private String accessToken;
+
+    /**
+     * Register a Rollbar bean to configure App with Rollbar.
+     */
+    @Bean
+    public Rollbar rollbar() {
+
+        // Your ACCESS TOKEN is:
+        // Make sure to keep this secure
+        return new Rollbar(getRollbarConfigs(accessToken));
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+
+        // Reference ConfigBuilder.java for all the properties you can set for Rollbar
+        return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                .environment("development")
+                .build();
+    }
+}
