8abb12089693743cf62a9e4d4b26bba1e64c8b29
MikhailKrylov <89261789846@mail.ru>
2022-12-08 21:32:49 +0300

    corrected some error with root page

--- src/main/java/hexlet/code/config/RollbarConfig.java
+++ src/main/java/hexlet/code/config/RollbarConfig.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration()
-@EnableWebMvc
 @ComponentScan({
         "hexlet.code",
         "com.rollbar.spring"
